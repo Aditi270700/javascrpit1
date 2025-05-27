236aeebbf23f588fdb2c84dafeb7296e04da2d0c
aditi saudagar <aditisaudagar17@gmail.com>
2025-05-27 18:34:16 +0530

    object

---
+++ script27.js
@@ -0,0 +1,28 @@
+// array destructuring
+
+const myArray = ["value1","value2","value3","value4"];
+// let myvar1 = myArray[0];
+// let myvar2 = myArray[1];
+// console.log("value of myvar1",myvar1);
+// console.log("value of myvar2",myvar2);
+
+// let [myvar1,myvar2,myvar3] = myArray;
+// console.log("value of myvar1",myvar1);
+// console.log("value of myvar2",myvar2);
+// console.log("value of myvar2",myvar3);
+
+// let [myvar1, ,myvar2] = myArray;
+// console.log("value of myvar1",myvar1);
+// console.log("value of myvar2",myvar2);
+
+// let [myvar1,myvar2] = myArray;
+// let myNewArray = myArray.slice(2);
+// console.log("value of myvar1",myvar1);
+// console.log("value of myvar2",myvar2);
+// console.log(myNewArray);
+
+// second type to generate array
+let [myvar1,myvar2,...myNewArray] = myArray;
+console.log("value of myvar1",myvar1);
+console.log("value of myvar2",myvar2);
+console.log(myNewArray);
---
+++ script28.js
@@ -0,0 +1,50 @@
+// objects
+// object is reference type
+
+// arrays are good but not sufficient
+// for real world data
+// objects store key value pairs
+// objects don't have index
+
+// how to create objects
+// const person = {name:"aditi",age:25};
+const person = {
+name:"aditi",
+age:25,
+hobbies:["dancing","singing","listening music"]
+}
+// we are not write key value in string but it is by default in string
+console.log(typeof person);
+// how to add key value pair to objects
+person.gender = "female";
+console.log(person);
+
+
+
+// how to access data from objects
+// this is dot notation
+console.log(person.name);
+console.log(person.age);
+
+
+// difference between dot and bracket notation
+// this is bracket notation
+console.log(person["name"]);
+console.log(person["age"]);
+
+
+// difference between dot and bracket notation
+const key = "email";
+const human = {
+name:"radhika",
+age:27,
+"person hobbies":["guitar","sleeping","listening music"]
+}
+// we don't use dot notation if space in key value
+console.log(human["person hobbies"]);
+// if we add new key in object
+human[key]="aditisaudagar17@gmail.com";
+console.log(human);
+
+
+
