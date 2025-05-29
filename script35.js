e4a34b98310aa77192d81a153f3059a24afe3293
aditi saudagar <aditisaudagar17@gmail.com>
2025-05-29 21:35:17 +0530

    function

---
+++ script35.js
@@ -0,0 +1,18 @@
+function singHappyBirthday(){
+console.log("happy birthday to you");
+}
+singHappyBirthday();
+singHappyBirthday();
+singHappyBirthday();
+singHappyBirthday();
+singHappyBirthday();
+
+function twoPlusFour(){
+console.log(2+4);
+}
+twoPlusFour();
+twoPlusFour();
+twoPlusFour();
+twoPlusFour();
+
+
